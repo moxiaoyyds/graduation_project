1de88b0f8802dc8703b2da2dd7375d92f53c9ee7
moxiaoyyds <1036282493@qq.com>
2022-10-08 16:08:45 +0800

    毕业设计P93,职称管理功能实现

--- yeb-server/src/main/java/cn/ybzy/server/controller/PositionController.java
+++ yeb-server/src/main/java/cn/ybzy/server/controller/PositionController.java
@@ -21,7 +21,7 @@ import java.util.List;
  * @since 2022-08-20
  */
 @RestController
-@RequestMapping("/system/config/pos")
+@RequestMapping("/system/basic/pos")
 public class PositionController {
 
     @Autowired
