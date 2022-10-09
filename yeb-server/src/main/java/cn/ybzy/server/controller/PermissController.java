cf273862e44d587e738f7d9fddceca78ca712229
moxiaoyyds <1036282493@qq.com>
2022-10-09 11:15:21 +0800

    毕业设计P94,权限组角色功能实现

---
+++ yeb-server/src/main/java/cn/ybzy/server/controller/PermissController.java
@@ -0,0 +1,51 @@
+package cn.ybzy.server.controller;
+
+import cn.ybzy.server.pojo.RespBean;
+import cn.ybzy.server.pojo.Role;
+import cn.ybzy.server.service.IRoleService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author wangHao
+ * @Description 权限组
+ * @createTime 2022-10-09 10:47
+ **/
+
+@RestController
+@RequestMapping("/system/basic/permiss")
+public class PermissController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @ApiOperation(value = "获取所有角色")
+    @GetMapping("/")
+    public List<Role> getAllRoles() {
+        return roleService.list();
+    }
+
+    @ApiOperation(value = "添加角色")
+    @PostMapping("/")
+    public RespBean addRole(@RequestBody Role role) {
+        if (!role.getName().startsWith("ROLE_")) {
+            role.setName("ROLE_" + role.getName());
+        }
+        if (roleService.save(role)) {
+            return RespBean.success("添加成功！");
+        }
+        return RespBean.error("添加失败！");
+    }
+
+    @ApiOperation(value = "删除角色")
+    @DeleteMapping("/role/{rid}")
+    public RespBean deleteRole(@PathVariable Integer rid) {
+        if (roleService.removeById(rid)) {
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+    }
+}
