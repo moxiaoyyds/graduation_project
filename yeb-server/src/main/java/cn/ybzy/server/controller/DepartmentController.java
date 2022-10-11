b8f8cdc80ce694c49fee677d2a24afad145273ae
moxiaoyyds <1036282493@qq.com>
2022-10-11 15:07:58 +0800

    毕业设计P102,获取所有部门、添加部门、删除部门

--- yeb-server/src/main/java/cn/ybzy/server/controller/DepartmentController.java
+++ yeb-server/src/main/java/cn/ybzy/server/controller/DepartmentController.java
@@ -1,9 +1,14 @@
 package cn.ybzy.server.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.ybzy.server.pojo.Department;
+import cn.ybzy.server.pojo.RespBean;
+import cn.ybzy.server.service.IDepartmentService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-08-20
  */
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/system/basic/department")
 public class DepartmentController {
 
+    @Autowired
+    private IDepartmentService departmentService;
+
+    @ApiOperation(value = "获取所有部门")
+    @GetMapping("/")
+    public List<Department> getAllDepartments() {
+        return departmentService.getAllDepartments();
+    }
+
+    @ApiOperation(value = "添加部门")
+    @PostMapping("/")
+    public RespBean addDep(@RequestBody Department dep) {
+        return departmentService.addDep(dep);
+    }
+
+    @ApiOperation(value = "删除部门")
+    @DeleteMapping("/{id}")
+    public RespBean deleteDep(@PathVariable Integer id) {
+        return departmentService.deleteDep(id);
+    }
 }
--- yeb-server/src/main/java/cn/ybzy/server/service/IDepartmentService.java
+++ yeb-server/src/main/java/cn/ybzy/server/service/IDepartmentService.java
@@ -1,7 +1,10 @@
 package cn.ybzy.server.service;
 
 import cn.ybzy.server.pojo.Department;
+import cn.ybzy.server.pojo.RespBean;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,23 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IDepartmentService extends IService<Department> {
 
+    /**
+     * 获取所有部门
+     * @return
+     */
+    List<Department> getAllDepartments();
+
+    /**
+     * 添加部门
+     * @param dep
+     * @return
+     */
+    RespBean addDep(Department dep);
+
+    /**
+     * 删除部门
+     * @param id
+     * @return
+     */
+    RespBean deleteDep(Integer id);
 }
